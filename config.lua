4333684be4145cf9f9de6a7a7a82e37bebcd93f2
adkun <adgnn@foxmail.com>
2023-03-18 16:43:09 +0800

    fix: [noice] disable override noice lsp hover and else...

--- config.lua
+++ config.lua
@@ -734,11 +734,11 @@ lvim.plugins = {
                         enabled = false,
                     },
 					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
-					},
+					-- override = {
+					-- 	["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					-- 	["vim.lsp.util.stylize_markdown"] = true,
+					-- 	["cmp.entry.get_documentation"] = true,
+					-- },
 				},
                 -- popupmenu = {
                 --     enabled = true,
